d0c2f95061f16a48991b7dafaf56a429ea41127f
Kien Ngoc Nguyen <tgsoon2002@gmail.com>
2017-10-27 13:54:44 -0700

    Add ability to go to create object, change scene

---
+++ Assets/Scripts/GamePlay/EnvironmentManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+
+    public GameObject platformPrefab;
+    public GameObject platformContainer;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MakePlatform();
+        }
+    }
+
+    private void MakePlatform()
+    {
+        GameObject temp = Instantiate(platformPrefab, platformContainer.transform.position,
+        Quaternion.identity, platformContainer.transform);
+    }
+}
