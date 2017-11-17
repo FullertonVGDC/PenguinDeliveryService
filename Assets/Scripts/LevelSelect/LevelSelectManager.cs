a81be154c317db26a3f0208aea7ad7f3f471b02e
Dylan Lang <dylanlang999@gmail.com>
2017-11-17 13:50:09 -0800

    Auto stash before merge of "Dylan" and "origin/Programer"

--- Assets/Scripts/LevelSelect/LevelSelectManager.cs
+++ Assets/Scripts/LevelSelect/LevelSelectManager.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.IO;
 
-
 public class LevelSelectManager : MonoBehaviour
 {
     // envirtoment objexct
     // Use this for initialization
     // Test out
-
     List<enviromentObject> myObject;
-    void Start()
-    {
+    void Start(){
+        MapBuilder();
+    }
+    public void MapBuilder(){
         myObject = new List<enviromentObject>();
         Debug.Log("..saving");
         
@@ -22,16 +22,17 @@ public class LevelSelectManager : MonoBehaviour
    
         // loop through the hieracrch 
         foreach (Transform child in transform) {
-            
             myObject.Add(new enviromentObject(true, child));
         }
         string json = JsonUtility.ToJson(myObject);
         Debug.Log(json);
+        Debug.Log(myObject);
+
+        writeJson.Close();
     }
 
-	// go to the level // scene
-    public void _GotoLevel()
-    {
+    // go to the level // scene
+    public void _GotoLevel(){
         SceneManager.LoadScene("GamePlayScene");
     }
 }
@@ -39,8 +40,7 @@ public class LevelSelectManager : MonoBehaviour
 public class enviromentObject {
     public bool isPassThrough;
     public Transform location;
-    public enviromentObject(bool isPass, Transform trans)
-    {
+    public enviromentObject(bool isPass, Transform trans){
         isPassThrough = isPass;
         location = trans;
     }
---
+++ Assets/Scripts/LevelSelect/ObjectBuilderEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelSelectManager))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI(){
+        DrawDefaultInspector();
+
+        LevelSelectManager myScript = (LevelSelectManager)target;
+        if (GUILayout.Button("Save Map"))
+        {
+            myScript.MapBuilder();
+        }
+    }
+}
