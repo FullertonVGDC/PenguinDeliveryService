af134c58e3c3e64d8e6fede6d3a9b07bec05bed3
kchang75 <kristi.chang@csu.fullerton.edu>
2017-12-01 16:11:39 -0800

    Add enemyMovement

---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	private bool dirRight = true;
+	public float speed = 3.0f;
+	private SpriteRenderer mySpriteRenderer;
+
+	private void Awake() {
+		mySpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+  
+	// Update is called once per frame
+	void Update () {
+		if (dirRight) {
+			transform.Translate (Vector2.right * speed * Time.deltaTime);
+			mySpriteRenderer.flipX = true;
+		}
+		else {
+			transform.Translate (-Vector2.right * speed * Time.deltaTime);
+			mySpriteRenderer.flipX = false;
+		}
+		
+		if(transform.position.x >= 4.0f) {
+			dirRight = false;
+		}
+		
+		if(transform.position.x <= -4) {
+			dirRight = true;
+		}
+	}
+
+	// void Attack() {
+	//
+	// }
+}
