50c528686285e84a986d85e33e40920745efeba3
kchang75 <kristi.chang@csu.fullerton.edu>
2018-04-08 02:08:33 -0700

    Change level layout, organize folders

---
+++ Assets/_Scripts/Menu/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void _MenuButton(int type){
+        switch (type)
+        {
+            case 0:
+                Application.Quit();
+                break;
+            case 1:
+                SceneManager.LoadScene("WorkingEnemy");
+                break;
+            default:
+                break;
+        }
+        
+	}
+    
+}
